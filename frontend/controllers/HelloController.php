e0f16482eb706be4382614d591205873e04445ae
李春鹏 <lcp0578@gmail.com>
2017-07-13 11:25:52 +0800

    [MOD] add hello

---
+++ frontend/controllers/HelloController.php
@@ -0,0 +1,13 @@
+<?php
+namespace frontend\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+class HelloController extends Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+}
--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -29,7 +29,7 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'My Company',
+        'brandLabel' => 'Yii2 demo',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
