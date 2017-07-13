0c13860146b5b8b89f1c915af9fa718c7374c42c
李春鹏 <lcp0578@gmail.com>
2017-07-13 15:50:28 +0800

    [MOD] 增加了actions

--- frontend/controllers/HelloController.php
+++ frontend/controllers/HelloController.php
@@ -6,8 +6,31 @@ use yii\web\Controller;
 
 class HelloController extends Controller
 {
+    // set default action
+    public $defaultAction = 'home-page';
+    /**
+     * (non-PHPdoc)
+     * @see \yii\base\Controller::actions()
+     * 
+     * 设置独立的动作
+     */
+    public function actions()
+    {
+        return [
+            'error' => 'yii\web\ErrorAction',
+            'view' => [
+                'class' => 'yii\web\ViewAction',
+                'viewPrefix' => ''
+            ]
+        ];
+    }
     public function actionIndex()
     {
         return $this->render('index');
+    }
+    
+    public function actionHomePage()
+    {
+        return $this->render('home-page');
     }
 }
---
+++ frontend/views/hello/home-page.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * @var $this yii\web\View
+ */
+$this->title = 'home page';
+echo 'home page';
