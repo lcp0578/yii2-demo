34547b9eb65adefb1da44172472d0394e3bed237
李春鹏 <lcp0578@gmail.com>
2017-07-14 18:51:17 +0800

    [MOD] add migrations

---
+++ console/migrations/m170714_105019_blog.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Migration;
+
+class m170714_105019_blog extends Migration
+{
+    public function safeUp()
+    {
+
+    }
+
+    public function safeDown()
+    {
+        echo "m170714_105019_blog cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m170714_105019_blog cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
